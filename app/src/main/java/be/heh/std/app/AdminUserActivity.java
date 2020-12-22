681309f3a701d319955204dd6fc5acbbd6c32fa1
teaflex <nicolas.jeuniaux.pro@hotmail.com>
2020-12-22 18:30:36 +0100

    Admin view began and constraint layout used in forms

---
+++ app/src/main/java/be/heh/std/app/AdminUserActivity.java
@@ -0,0 +1,14 @@
+package be.heh.std.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdminUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_user);
+    }
+}
--- app/src/main/java/be/heh/std/app/FormActivity.java
+++ app/src/main/java/be/heh/std/app/FormActivity.java
@@ -57,8 +57,7 @@ public abstract class FormActivity extends Activity {
         for (View v: notEmptyInputs) {
             //Initially check if the text fields that must be filled are not empty.
             if((v instanceof EditText) && (isEditTextEmpty((EditText) v)))
-                throw new Exception(String.format(getString(R.string.empty_err),
-                        ((EditText)v).getHint()));
+                throw new Exception(getString(R.string.empty_err, ((EditText)v).getHint()));
         }
     }
 
@@ -68,8 +67,7 @@ public abstract class FormActivity extends Activity {
 
     public void verifyLength(EditText in, int min, int max) throws Exception {
         if(in.getText().length() < min || in.getText().length() > max)
-            throw new Exception(String.format(getString(R.string.between_err),
-                    in.getHint(), min, max));
+            throw new Exception(getString(R.string.between_err, in.getHint(), min, max));
     }
 
     public void verifyEmail(EditText in) throws Exception {
@@ -79,14 +77,12 @@ public abstract class FormActivity extends Activity {
 
     public void verifyPassword(EditText in) throws Exception {
         if(!Pattern.matches("^(?=.*[0-9])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$", in.getText()))
-            throw new Exception(String.format(getString(R.string.bad_password_err),
-                    in.getHint(), 8, 1, 1));
+            throw new Exception(getString(R.string.bad_password_err, in.getHint(), 8, 1, 1));
     }
 
     public void areTextMatching(EditText one, EditText two) throws Exception {
         if(!one.getText().toString().equals(two.getText().toString()))
-            throw new Exception(String.format(getString(R.string.dont_match_err),
-                    one.getHint(), two.getHint()));
+            throw new Exception(getString(R.string.dont_match_err, one.getHint(), two.getHint()));
     }
 
     @RequiresApi(api = Build.VERSION_CODES.O)
